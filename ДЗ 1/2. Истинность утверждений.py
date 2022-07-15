1c73909930d9b575beee1d7b02d2ad7e35503540
SenEvgYur <102899734+SenEvgYur@users.noreply.github.com>
2022-07-15 21:30:15 +0300

    Решено ДЗ 1 и начато ДЗ 2

--- ДЗ 1/2. Истинность утверждений.py
+++ ДЗ 1/2. Истинность утверждений.py
@@ -1,18 +1,14 @@
 # Напишите программу для. проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
+# ¬ - not
+# ⋁ - or
+# ⋀ - and
 
-rezult = True
-print ('x',  'y',  'z', ) 
+print('  x      y      z   Результат') 
+print('-----------------------------')
 for x in True, False:
     for y in True, False:
         for z in True, False:
             if not(x or y or z) == (not x and not y and not z):
-                rezult = rezult and (not(x or y or z) == (not x and not y and not z))
-print(rezult)
-
-
-
-# ¬ - not
-# ⋁ - or
-# ⋀ - and
+                print(f'{x}   {y}   {z}   {(not(x or y or z) == (not x and not y and not z))}')
