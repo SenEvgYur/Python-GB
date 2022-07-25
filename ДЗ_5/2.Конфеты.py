88a602c80194ec7a3e7e93a9e7358d65c711f13e
SenEvgYur <es@kraski.ru>
2022-07-26 02:56:31 +0300

    Начато решение игры - конфеты

---
+++ ДЗ_5/2.Конфеты.py
@@ -0,0 +1,54 @@
+board = 99
+
+wins_cords = (board == 0)
+
+def draw_board():
+    print('------------')
+    print('|   ', board,'   |')
+    print('------------')
+
+
+def take_input(znak):
+    while True:
+        value = input('Куда поставить: ' +znak + '? ')
+        if not (value in '123456789'):
+            print('Ошибочный ввод. Повторите набор')
+            continue
+        value = int(value)
+        if str(board[value - 1]) in 'XO':
+            print('Эта клетка уже занята')
+            continue
+        board[value - 1] = znak
+        break
+
+
+def check_win():
+    for item in wins_cords:
+        if (board[item[0]-1]) == (board[item[1]-1]) == (board[item[2]-1]):
+            return board[item[1]-1]
+    else:
+        return False
+
+
+def igra():
+    hod = 0
+    while True:
+        draw_board()
+        if hod % 2 == 0:
+            take_input('X')
+        else:
+            take_input('O')
+        if hod > 3:
+            winner = check_win()
+            if winner:
+                draw_board()
+                print(winner, 'Победа !!!')
+                break
+        hod += 1
+        if hod > 8:
+            draw_board()
+            print('Ничья')
+            break
+
+
+draw_board()
