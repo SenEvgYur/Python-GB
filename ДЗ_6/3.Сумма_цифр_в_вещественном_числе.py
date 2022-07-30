7064a14c2b88d2dd8287be25b584575b44a57e52
SenEvgYur <es@kraski.ru>
2022-07-31 01:06:38 +0300

    Добавлено ДЗ_6

---
+++ ДЗ_6/3.Сумма_цифр_в_вещественном_числе.py
@@ -0,0 +1,31 @@
+# Напишите программу, которая принимает на вход
+# вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+# x = float(input('Введите вещественное число: '))
+# x = abs(x)
+
+# while ((x % 1 > 0) and (x % 1 < 1)):
+#     x *= 10
+
+# summa_chisel = x % 10
+# while (x % 100 > 10):
+#     x = int(x / 10)
+#     summa_chisel += x % 10
+
+# summa_chisel = int(summa_chisel)
+# print(f'Сумма цифр в числе = {summa_chisel}')   
+from random import uniform
+
+x = float(input('Введите вещественное число: '))
+x = abs(x)
+
+def sum_digit(x):
+    return sum(map(int, list(str(x).replace('.',''))))
+
+print(x)
+print(sum_digit(x)) 
---
+++ ДЗ_6/5.Произведение_пар_чисел.py
@@ -0,0 +1,33 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+arr = input('Введите числа списка через пробел: ')
+arr = arr.split()
+my_list = []
+
+for i in arr:
+    i = int(i)
+    my_list.append(i)
+
+mul = lambda x, y: x*y
+
+print(my_list)
+
+# if len(my_list) % 2 != 0:
+#     for i in range(round(len(my_list)/2)+1):
+#         print(f'{my_list[i]} * {(len(my_list)-i)} = {my_list[i] * (len(my_list)-i)}')
+# else:
+#     for i in range(round(len(my_list)/2)):
+#         print(f'{my_list[i]} * {(len(my_list)-i)} = {my_list[i] * (len(my_list)-i)}')
+ 
+if len(my_list) % 2 != 0:
+    for i in range(round(len(my_list)/2)+1):
+        print(mul(my_list[i], len(my_list)-i))
+else:
+    for i in range(round(len(my_list)/2)):
+        print(mul(my_list[i], len(my_list)-i))
