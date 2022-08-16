fae8bc7a07cdb0f36900fc0bd415c27d0cdcd6d6
SenEvgYur <es@kraski.ru>
2022-08-16 17:46:00 +0300

    Update lesson_2.py

--- Tkinter/lesson_2.py
+++ Tkinter/lesson_2.py
@@ -1,8 +1,25 @@
 # Знакомство с виджетами. Виджет Label
 
+from cProfile import label
 import tkinter as tk
 
 win = tk.Tk()
+win.geometry('300x400+100+100')
 win.title('Моё первое графическое приложение')
-win.geometry('500x500+100+100')
+label_1 = tk.Label(win, text = '''Hello 
+                    world!''',
+                    bg = 'red', # Back Ground
+                    fg = 'white', # Font Ground
+                    font = ('Arial', 15, 'bold'), # шрифт лэйбла
+                    # padx = 30, # отступы внутри окна
+                    # pady = 40 # отступы внутри окна
+                    width = 20, # ширина лэйбла
+                    height = 10, # высота лэйбла
+                    anchor = 'sw', # расположение лейбла внутри своего окна, по сторонам света
+                    relief = tk.RAISED, # рельеф окна
+                    bd = 10, # ширина рельефа окна
+                    justify = tk.RIGHT # выравнивание текста
+                    )
+label_1.pack()
+
 win.mainloop()
